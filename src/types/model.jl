53ec1cc5118ad20cd7415b8fa7dc419f9692cd66
Sunoru <s@sunoru.com>
2022-02-01 15:07:20 -0500

    Define has_pbc as box equaling zero.

--- src/types/model.jl
+++ src/types/model.jl
@@ -13,8 +13,8 @@ force_function(::MosiModel, rs, i) = error("Unimplemented")
 potential_energy_gradients(model::MosiModel, rs) = -force_function(model, rs)
 potential_energy_gradients(model::MosiModel, rs, i) = -force_function(model, rs, i)
 
-has_pbc(model::MosiModel) = false
 pbc_box(model::MosiModel) = model.box
+has_pbc(model::MosiModel{T}) where T = pbc_box(model) ≡ zero(T)
 distance_function(model_or_system::Union{MosiModel, MosiSystem}) = if has_pbc(model_or_system)
     pbc_distance(pbc_box(model_or_system))
 else
