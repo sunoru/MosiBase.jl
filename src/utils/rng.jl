f20a5736cccbb7bf12aa6ccbc51ccb3288fa4c0d
Sunoru <s@sunoru.com>
2022-03-31 19:08:36 -0400

    Use our own make_seed.

--- src/utils/rng.jl
+++ src/utils/rng.jl
@@ -1,4 +1,5 @@
-using Random: make_seed, Xoshiro
+import Random
+using Random: Xoshiro
 
 const MosiRNG = Xoshiro
 
@@ -7,3 +8,6 @@ new_rng(seed) = MosiRNG(seed)
 restore_rng(state::NTuple{4, UInt64}) = MosiRNG(state...)
 
 rng_state(rng::MosiRNG) = (rng.s0, rng.s1, rng.s2, rng.s3)
+
+make_seed() = UInt64(Random.make_seed()[1])
+make_seed(n::Integer) = UInt64(Random.make_seed(n)[1])
